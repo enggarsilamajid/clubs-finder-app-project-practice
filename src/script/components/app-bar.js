db212a745a8aa87a5b9d43cdfdc9baf216633a2b
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-02-19 10:13:19 +0700

    update

---
+++ src/script/components/app-bar.js
@@ -0,0 +1,11 @@
+class AppBar extends HTMLElement {
+    _shadowRoot = null;
+    _style = null;
+
+    constructor() {
+        super();
+
+        this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this._style = document.cre
+    }
+}
